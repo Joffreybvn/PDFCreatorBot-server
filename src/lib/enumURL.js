e9ac5d5fbf0a86e73068ee2967a41d12c017d9b0
Joffrey4 <joffreybvn@gmail.com>
2017-08-09 15:03:42 +0200

    remote links updated

--- src/lib/enumURL.js
+++ src/lib/enumURL.js
@@ -1,5 +1,5 @@
 "use strict";
 
 module.exports =  {
-    SERV_STATIC: {local: "http://127.0.0.1:3000/", remote: "http://botpdfcreator-static.azurewebsites.net/"}
+    SERV_STATIC: {local: "http://127.0.0.1:3000/", remote: "https://botpdfcreator-static.azurewebsites.net/"}
 };
